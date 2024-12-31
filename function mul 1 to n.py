da6aee298aff2ff0130a4c8732706b72023bcf2e
TABASSUM-SD <tabasumtabasum849@gmail.com>
2024-12-31 11:06:55 +0530

    Add files via upload

---
+++ function mul 1 to n.py
@@ -0,0 +1,9 @@
+def print_table(num):
+   for i in range (1,num+1):
+         print("--------------")
+         for j in range(1,11):
+           print(i,"x",j,"=",i*j)
+result=print_table(5) 
+print(result)
+
+   
